ec8804905508dbf02f205bbb2601b5bdaf8f32d5
明柯 阳 <ouyangmingke@gmail.com>
2020-12-14 08:58:11 +0800

    我点修啊给I

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,6 +13,8 @@ namespace ConsoleApp1
             Console.WriteLine("添加");
             Console.WriteLine("ddsds");
             Console.WriteLine("点击修改");
+            Console.WriteLine("我不点修改看看吧");
+            Console.WriteLine("我的点修改看看");
             Console.Read();
 
             
