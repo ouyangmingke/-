4a1b6906c33603679f95259c13589a31a2083f83
明柯 阳 <ouyangmingke@gmail.com>
2020-12-14 11:46:30 +0800

    hias tijiao

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("添加");
-            Console.WriteLine("添加");
+            Console.WriteLine("添加"); Console.WriteLine("添加");
             Console.WriteLine("添加"); Console.WriteLine("添加"); Console.WriteLine("添加");
             Console.WriteLine("添加");
 
